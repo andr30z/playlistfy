11bd91660e1dd76a2c757934a7d29c0d6468919b
andr30z <51346021+andr30z@users.noreply.github.com>
2022-10-27 17:06:06 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "playlistfy-api.up.railway.app/"
+    ? "https://playlistfy-api.up.railway.app/"
     : "http://localhost:3333/";
 const auth = axios.create({
   baseURL: API_URL,
