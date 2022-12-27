3201290a5426a166b14a40af15462bad1ab4a0e1
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2022-12-27 17:35:41 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://playlistfy-backend.onrender.com"
+    ? "https://playlistfy-backend.onrender.com/"
     : "http://localhost:3333/";
 const auth = axios.create({
   baseURL: API_URL,
