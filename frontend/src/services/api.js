5a5ac8a23e5139ca09f64ab5dcc25b6b5bf82f55
andr30z <51346021+andr30z@users.noreply.github.com>
2023-07-17 12:58:23 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://playlistfy-backend.onrender.com/"
+    ? "https://playlistfy.vercel.app/"
     : "http://localhost:3333/";
 const auth = axios.create({
   baseURL: API_URL,
